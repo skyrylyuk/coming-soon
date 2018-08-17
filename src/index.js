1b4873c75e11d21c506b951b3f72829871c9cabe
Sergii Kyryliuk <serhiy.kyrylyuk@gmail.com>
2018-08-17 23:41:59 +0300

    Init commit

---
+++ src/index.js
@@ -0,0 +1,3 @@
+var element = document.createElement('div');
+element.innerHTML = "Welcome to the Bendywork's webpack demo!!!";
+document.body.appendChild(element);
---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+var path = require('path');
+const webpack = require("webpack");
+// const HtmlWebpackPlugin = require("html-webpack-plugin");
+ 
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'build'),
+    publicPath: "build/"
+  },
+  plugins: [
+    // new HtmlWebpackPlugin()
+  ]
+};
